ce78f45c24b5640dd509a8f680ff32f5d065d557
yva <whiteodmin@gmail.com>
2016-11-22 00:06:03 +0200

    1

---
+++ models/tables.py
@@ -0,0 +1,73 @@
+from ext.databases import Base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+
+
+
+class SUser(Base):
+    __tablename__ = 'sip_buddies'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), unique=True)
+    callerid = Column(String(80), unique=False)
+    defaultuser= Column(String(80), unique=True)
+    regexten= Column(String(80), unique=False)
+    secret= Column(String(80), unique=True)
+    accountcode= Column(String(20), unique=False)
+    context= Column(String(80), unique=False)
+    amaflags= Column(String(7), unique=False)
+    callgroup= Column(String(10), unique=True)
+    canreinvite= Column(String(3), unique=False)
+    defaultip= Column(String(15), unique=False)
+    dtmfmode= Column(String(7), unique=False)
+    fromuser= Column(String(80), unique=False)
+    fromdomain= Column(String(80), unique=False)
+    fullcontact= Column(String(80), unique=True)
+    host= Column(String(31), unique=False)
+    insecure= Column(String(4), unique=False)
+    mailbox= Column(String(50), unique=False)
+    md5secret= Column(String(80), unique=True)
+    nat= Column(String(5), unique=False)
+    deny= Column(String(95), unique=False)
+    permit= Column(String(95), unique=False)
+    mask= Column(String(95), unique=False)
+    pickupgroup= Column(String(10), unique=False)
+    port= Column(String(5), unique=False)
+    qualify= Column(String(3), unique=False)
+    restrictcid= Column(String(1), unique=False)
+    rtptimeout= Column(String(3), unique=False)
+    rtpholdtimeout= Column(String(3), unique=False)
+    type= Column(String(6), unique=False)
+    disallow= Column(String(100), unique=False)
+    allow= Column(String(100), unique=False)
+    musiconhold= Column(String(100), unique=False)
+    regseconds= Column(Integer, unique=False)
+    ipaddr= Column(String(15), unique=False)
+    cancallforward= Column(String(3), unique=False)
+    lastms= Column(Integer, unique=False)
+    useragent= Column(String(255), unique=False)
+    regserver= Column(String(100), unique=False)
+    lastmoddate= Column(DateTime, unique=False)#!
+    lastmoduser= Column(String(100), unique=False)
+    access= Column(String(100), unique=False)
+    callbackextension= Column(String(100), unique=False)
+    calllimit = Column(Integer, unique=False)
+
+    def __init__(self, name=None, callerid=None):
+        self.name = name
+        self.callerid = callerid
+
+    def __repr__(self):
+        return '%s %s %s' % (self.name,self.callerid,self.fullcontact)
+
+
+#class TExt(Base):
+#    _tablename__ = 'extensions'
+#
+#    def __init__(self,x):
+#        x=None
+
+
+#class TCdr(Base):
+#    _tablename__ = 'cdr'
+#
+#    def __init__(self, x):
+#        x = None
---
+++ one.py
@@ -0,0 +1,137 @@
+from flask import Flask,render_template, request,jsonify
+from flask import abort
+from jinja2 import TemplateNotFound
+import random
+import simplejson as json
+#import tables
+
+app = Flask(__name__,template_folder='templates')
+
+@app.route('/')
+def form():
+    return render_template('form_submit.html')
+
+
+@app.route('/search/', methods=['POST'])
+def search():
+
+    name=request.form['yourname']
+    sip_user_result=tables.SUser.query.filter(tables.SUser.name == name).first()
+    check=request.form['check']
+    try:
+        sip_user_result.name==name
+    except AttributeError:
+        return render_template('form_submit.html',error='not found')
+    if (check=='2'):
+
+        return render_template('table_sip.html',id=sip_user_result.id
+                           , accountcode= sip_user_result.accountcode
+                           , disallow= sip_user_result.disallow
+                           , allow= sip_user_result.allow
+                           #, allowoverlap= sip_user_result.allowoverlap
+                           #, allowsubscribe= sip_user_result.allowsubscribe
+                           #, allowtransfer= sip_user_result.allowtransfer
+                           , amaflags= sip_user_result.amaflags
+                           #, autoframing= sip_user_result.autoframing
+                           #, auth= sip_user_result.auth
+                           #, buggymwi= sip_user_result.buggymwi
+                           , callgroup= sip_user_result.callgroup
+                           , callerid= sip_user_result.callerid
+                           #, cid_number= sip_user_result.cid_number
+                           #, fullname= sip_user_result.fullname
+                           , calllimit= sip_user_result.calllimit
+                           #, callingpres= sip_user_result.callingpres
+                           , canreinvite= sip_user_result.canreinvite
+                           , context= sip_user_result.context
+                           , defaultip= sip_user_result.defaultip
+                           , dtmfmode= sip_user_result.dtmfmode
+                           , fromuser= sip_user_result.fromuser
+                           , fromdomain= sip_user_result.fromdomain
+                           , fullcontact= sip_user_result.fullcontact
+                           #, g726nonstandard= sip_user_result.g726nonstandard
+                           , host= sip_user_result.host
+                           , insecure= sip_user_result.insecure
+                           , ipaddr= sip_user_result.ipaddr
+                           #, language= sip_user_result.language
+                           , lastms= sip_user_result.lastms
+                           , mailbox= sip_user_result.mailbox
+                           #, maxcallbitrate= sip_user_result.maxcallbitrate
+                           #, mohsuggest= sip_user_result.mohsuggest
+                           , md5secret= sip_user_result.md5secret
+                           , name= sip_user_result.name
+                           , nat= sip_user_result.nat
+                           #, outboundproxy= sip_user_result.outboundproxy
+                           , deny= sip_user_result.deny
+                           , permit= sip_user_result.permit
+                           , pickupgroup= sip_user_result.pickupgroup
+                           , port= sip_user_result.port
+                           #, progressinband= sip_user_result.progressinband
+                           #, promiscredir= sip_user_result.promiscredir
+                           , qualify= sip_user_result.qualify
+                           , regexten= sip_user_result.regexten
+                           , regseconds= sip_user_result.regseconds
+                           #, rfc2833compensate= sip_user_result.rfc2833compensate
+                           , rtptimeout= sip_user_result.rtptimeout
+                           , rtpholdtimeout= sip_user_result.rtpholdtimeout
+                           , secret= sip_user_result.secret
+                           #, setvar= sip_user_result.setvar
+                           #, subscribecontext= sip_user_result.subscribecontext
+                           #, subscribemwi= sip_user_result.subscribemwi
+                           #, t38pt_udptl= sip_user_result.t38pt_udptl
+                           #, trustrpid= sip_user_result.trustrpid
+                           , type= sip_user_result.type
+                           #, useclientcode= sip_user_result.useclientcode
+                           #, username= sip_user_result.username
+                           #, usereqphone= sip_user_result.usereqphone
+                           #, videosupport= sip_user_result.videosupport
+                           #, vmexten= sip_user_result.vmexten
+                           , lastmoddate= sip_user_result.lastmoddate
+                           , lastmoduser= sip_user_result.lastmoduser
+                           , access= sip_user_result.access)
+    else:
+        return render_template('short_sip.html',id= sip_user_result.id
+                           , name= sip_user_result.name
+                           , secret= sip_user_result.secret
+                           , host= sip_user_result.host
+                           , type= sip_user_result.type
+                           , callerid= sip_user_result.callerid
+                           , accountcode= sip_user_result.accountcode
+                           , allow= sip_user_result.allow
+                           , calllimit= sip_user_result.calllimit
+                           , nat= sip_user_result.nat
+                           , permit= sip_user_result.permit
+                           , deny= sip_user_result.deny
+                           , ipaddr= sip_user_result.ipaddr
+                           , port= sip_user_result.port
+                           , fullcontact= sip_user_result.fullcontact
+                           , lastms= sip_user_result.lastms
+                           , regseconds= sip_user_result.regseconds
+                           , lastmoddate= sip_user_result.lastmoddate
+                           , lastmoduser= sip_user_result.lastmoduser
+                           , access= sip_user_result.access)
+@app.route('/palette/')
+def table():
+    json_helper = {}
+    json_helper['id'] = 1 # exten_result=tables.TExt.query.filter(tables.TExt.id == id).first()
+    json_helper['context'] = 'default' # exten_result=tables.TExt.query.filter(tables.TExt.context == context).first()
+    json_helper['exten'] = '_X.' # exten_result=tables.TExt.query.filter(tables.TExt.exten == exten).first()
+    json_helper['priority'] = 1 # exten_result=tables.TExt.query.filter(tables.TExt.priority == priority).first()
+    json_helper['app'] = 'Dial' # exten_result=tables.TExt.query.filter(tables.TExt.app == app).first()
+    json_helper['appdata'] = 'SIP/${EXTEN}' # exten_result=tables.TExt.query.filter(tables.TExt.appdata == appdata).first()
+    json_helper['lastmodname'] = 'admin'  # exten_result=tables.TExt.query.filter(tables.TExt.lastmodname == lastmodname).first()
+    json_helper['lastmoddate'] = '01-01-2012 11:01:00' # exten_result=tables.TExt.query.filter(tables.TExt.lastmoddate == lastmoddate).first()
+    json_helper['access'] = 'admin' # exten_result=tables.TExt.query.filter(tables.TExt.access == access).first()  
+    json_object = json.dumps(json_helper)
+    return render_template('palette.html',s_data=json_object)
+
+@app.route('/<page>/')
+def show(page):
+    try:
+        return render_template('%s.html' % page)
+    except TemplateNotFound:
+        abort(404)
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
+
