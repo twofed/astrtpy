d655849168bc5599169506a816057c71de0844b4
twofed <whitepapugai@gmail.com>
2017-03-20 16:55:18 +0200

    DB.
    Вынес подключение, на будущее унести в один конфиг.

--- models/databases.py
+++ models/databases.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('mysql://asterisk:2843@localhost/asterisk?charset=utf8', convert_unicode=True)
+srv_connect = 'mysql://asterisk:2843@localhost/asterisk?charset=utf8';
+
+engine = create_engine(srv_connect, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
